8011fd30e4514a8b1fd3bfb3db5335b458e0b849
audiotrash112@gmail.com <audiotrash112@gmail.com>
2017-05-29 16:58:12 -0400

    Initial commit

---
+++ app/src/main/java/mymaps/com/example/admin/mymaps/MapsActivity.java
@@ -0,0 +1,63 @@
+package mymaps.com.example.admin.mymaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MapsActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
+
+    //private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        StreetViewPanoramaFragment mapFragment = (StreetViewPanoramaFragment) getFragmentManager()
+                .findFragmentById(R.id.maps);
+        mapFragment.getStreetViewPanoramaAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+//    @Override normal maps 
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        LatLng home = new LatLng(43.733215, -79.261415);
+        panorama.setPosition(home);
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera
+                .Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera,1000);
+
+    }
+}
